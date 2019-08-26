8b6996a42880cfa84ce094219dd82a30db131c27
sertmer <scottertmer@gmail.com>
2019-08-26 08:01:13 -0600

    Complete 6/7 tests

---
+++ exercises/dragon.js
@@ -0,0 +1,10 @@
+class Dragon {
+  constructor(name, rider, color) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.hungry = true;
+  }
+  
+}
+module.exports = Dragon;
