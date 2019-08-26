bdd9cb9786889b45b08d4c79f724a7907897cfcd
sertmer <scottertmer@gmail.com>
2019-08-26 08:41:13 -0600

    Complete dragon

--- exercises/dragon.js
+++ exercises/dragon.js
@@ -4,7 +4,19 @@ class Dragon {
     this.rider = rider;
     this.color = color;
     this.hungry = true;
+    this.counter = 0;
   }
-  
+  // eat function should loop
+  // three times before exiting
+  // How to implement array here?
+  // way off track?
+  eat() {
+    this.counter++;
+    if (this.counter > 2) {
+      this.hungry = false;
+    }
+  }
 }
+
+
 module.exports = Dragon;
