4579c7c107a845b981eebc9d32213bd5ccd44d51
sertmer <scottertmer@gmail.com>
2019-09-02 14:43:48 -0600

    Complete 10/11 tests

---
+++ exercises/medusa.js
@@ -0,0 +1,16 @@
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  stare(victim) {
+    this.statues.push(victim);
+    console.log(victim);
+    victim.isStoned = true;
+    if (this.statues.length > 3) {
+      this.statues = this.statues.slice(0, 3);
+    }
+  }
+}
+
+module.exports = Medusa
---
+++ exercises/person.js
@@ -0,0 +1,8 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.isStoned = false;
+  }
+}
+
+module.exports = Person
